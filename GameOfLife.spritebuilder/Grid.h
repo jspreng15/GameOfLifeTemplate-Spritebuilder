1a11fd3b1d0574e30c2338a1204218005f8eafc1
jspreng15 <jack_spreng@JackMac.local>
2014-07-23 00:25:16 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -7,10 +7,17 @@
 //
 
 #import "CCSprite.h"
+#import "Creature.h"
 
 @interface Grid : CCSprite
 
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
+
+-(void) evolveStep;
+-(void) countNeighbors;
+-(void) updateCreatures;
+-(BOOL)isIndexValidForX: (int) x andY: (int) y;
+
 
 @end
